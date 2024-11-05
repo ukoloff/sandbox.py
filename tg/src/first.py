36d88fe78bde47336fd9c88e07f18abc98ce8654
Stas Ukolov <ukoloff@gmail.com>
2024-11-05 10:06:56 +0500

    Add echo handler

--- tg/src/first.py
+++ tg/src/first.py
@@ -2,7 +2,13 @@ import os
 
 import logging
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import (
+    filters,
+    ApplicationBuilder,
+    ContextTypes,
+    CommandHandler,
+    MessageHandler,
+)
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -15,12 +21,21 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
 
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text=update.message.text
+    )
+
+
 def main():
-    token = open(os.path.join(__file__, '../.token')).readline().strip()
+    token = open(os.path.join(__file__, "../.token")).readline().strip()
     application = ApplicationBuilder().token(token).build()
 
     start_handler = CommandHandler("start", start)
     application.add_handler(start_handler)
+
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    application.add_handler(echo_handler)
 
     application.run_polling()
 
