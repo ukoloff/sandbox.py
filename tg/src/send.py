0767591bbf51f424cf6d88ec2f5fd6258814ae43
Stas Ukolov <ukoloff@gmail.com>
2024-11-06 10:03:06 +0500

    Upgrade to Supergroup

--- tg/src/send.py
+++ tg/src/send.py
@@ -5,10 +5,11 @@ import os
 import requests
 
 user_id = 305707966
-group_id = -4526893000
+sgroup_id = -1002250800558
 
 token = open(os.path.join(__file__, "../.token")).readline().strip()
 
-data = {"chat_id": group_id, "text": "Проверяю отправку оповещений ботом"}
+data = {"chat_id": sgroup_id, "text": "Проверяю отправку оповещений ботом"}
 url = f"https://api.telegram.org/bot{token}/sendMessage"
 req = requests.post(url, data)
+print(req)
