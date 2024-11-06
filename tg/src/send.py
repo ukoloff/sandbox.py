407c3a185ead306b7ecf0b6472ec45026f8e2851
Stas Ukolov <ukoloff@gmail.com>
2024-11-06 10:00:12 +0500

    Send to group

--- tg/src/send.py
+++ tg/src/send.py
@@ -5,9 +5,10 @@ import os
 import requests
 
 user_id = 305707966
+group_id = -4526893000
 
 token = open(os.path.join(__file__, "../.token")).readline().strip()
 
-data = {"chat_id": user_id, "text": "Проверяю отправку оповещений ботом"}
+data = {"chat_id": group_id, "text": "Проверяю отправку оповещений ботом"}
 url = f"https://api.telegram.org/bot{token}/sendMessage"
 req = requests.post(url, data)
