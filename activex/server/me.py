4e94bad65303c5660e44439545d38d4c8758773c
Stas Ukolov <ukoloff@gmail.com>
2024-11-12 15:32:38 +0500

    Register Server

--- activex/server/me.py
+++ activex/server/me.py
@@ -2,6 +2,10 @@
 # ActiveX server
 #
 class HelloWorld:
+    _reg_clsid_ = "{3DBDDE7C-9898-4BEE-B9B6-72B532AFFD00}"
+    _reg_desc_ = "Python Test COM Server"
+    _reg_progid_ = "ukoloff.test"
+
     _public_methods_ = ["Hello"]
     _public_attrs_ = ["softspace", "noCalls"]
     _readonly_attrs_ = ["noCalls"]
@@ -14,3 +18,13 @@ class HelloWorld:
         self.noCalls = self.noCalls + 1
         # insert "softspace" number of spaces
         return "Hello" + " " * self.softspace + str(who)
+
+
+def main():
+    import win32com.server.register
+
+    win32com.server.register.UseCommandLine(HelloWorld)
+
+
+if __name__ == "__main__":
+    main()
