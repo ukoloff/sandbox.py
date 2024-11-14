c872c5417e7b25d72a0d4e7631a7d3ad5b723dad
Stas Ukolov <ukoloff@gmail.com>
2024-11-14 13:30:24 +0500

    Save XLSX

--- z23/korstat/korstat.py
+++ z23/korstat/korstat.py
@@ -3,8 +3,13 @@
 #
 import os
 from dbfread import DBF
+from pandas import DataFrame
 
 src = os.path.join(__file__, '../data/KORSTAT_5.DBF')
 
 data = DBF(src, encoding='cp866')
-print(*data)
+frame = DataFrame(iter(data))
+# print(frame)
+
+dst = os.path.join(__file__, '../../../tmp/stat.xlsx')
+frame.to_excel(dst)
