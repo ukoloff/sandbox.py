44fa1cf29b56e74a3f56dec885feee19feeff328
Stas Ukolov <ukoloff@gmail.com>
2024-11-14 14:05:40 +0500

    Remove numbering

--- z23/korstat/korstat.py
+++ z23/korstat/korstat.py
@@ -14,4 +14,4 @@ frame = pandas.DataFrame(iter(data))
 dst = os.path.join(__file__, "../../../tmp/stat.xlsx")
 if os.path.isfile(dst):
     load = pandas.read_excel(dst)
-frame.to_excel(dst, freeze_panes=(1, 0))
+frame.to_excel(dst, freeze_panes=(1, 0), index=False)
