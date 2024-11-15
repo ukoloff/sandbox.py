38da1bbc72f975fe62258c6a7da115ab5c30e548
Stas Ukolov <ukoloff@gmail.com>
2024-11-15 09:22:43 +0500

    Format date[time]

--- z23/korstat/korstat.py
+++ z23/korstat/korstat.py
@@ -3,6 +3,7 @@
 #
 import os
 import json
+from datetime import date, datetime
 import openpyxl
 from dbfread import DBF
 import openpyxl.styles
@@ -12,6 +13,16 @@ dst = r"\\kzkserv\Data\Baza_23\stat\stat.xlsx"
 
 dst = os.path.join(__file__, "../../../tmp/stat.xlsx")
 
+
+# https://stackoverflow.com/a/22238613/6127481
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    raise TypeError("Type %s not serializable" % type(obj))
+
+
 if not os.path.isfile(src):
     print("File not found: ", src)
     exit()
@@ -20,7 +31,7 @@ data = DBF(src, encoding="cp866")
 
 wb = openpyxl.Workbook()
 ws = wb.active
-ws.title = 'korStat'
+ws.title = "korStat"
 
 first = True
 
@@ -28,11 +39,11 @@ for row in data:
     if first:
         ws.append(list(row.keys()))
         font = openpyxl.styles.Font(bold=True)
-        align = openpyxl.styles.Alignment(horizontal='center')
+        align = openpyxl.styles.Alignment(horizontal="center")
         for cell in ws[1]:
             cell.font = font
             cell.alignment = align
-        ws.freeze_panes = 'A2'
+        ws.freeze_panes = "A2"
         first = False
     ws.append(list(row.values()))
 
@@ -41,4 +52,4 @@ wb.save(dst)
 wb = openpyxl.load_workbook(dst, read_only=True, data_only=True)
 ws = wb.active
 for row in ws:
-    print(json.dumps([cell.value for cell in row], ensure_ascii=False, default=str))
+    print(json.dumps([cell.value for cell in row], ensure_ascii=False, default=json_serial))
