f0f2405adf0aaf16b4a2eec77cee902d960bb27b
Stas Ukolov <ukoloff@gmail.com>
2024-11-15 08:54:03 +0500

    Drop pandas

--- z23/korstat/korstat.py
+++ z23/korstat/korstat.py
@@ -2,34 +2,24 @@
 # Перенос записей из C:\Paket\Baza\KorStat.dbf в \\kzkserv\Data\Baza_23\stat\stat.XLSX
 #
 import os
+import openpyxl
 from dbfread import DBF
-import pandas
 
 src = r"C:\Paket\Baza\KorStat.dbf"
 dst = r"\\kzkserv\Data\Baza_23\stat\stat.xlsx"
+
+dst = os.path.join(__file__, "../../../tmp/stat.xlsx")
 
 if not os.path.isfile(src):
     print("File not found: ", src)
     exit()
 
 data = DBF(src, encoding="cp866")
-frame = pandas.DataFrame(iter(data))
-frame = frame.convert_dtypes()
 
-prev = 0
+wb = openpyxl.Workbook()
+ws = wb.active
 
-if os.path.isfile(dst):
-    prev = 1
-    load = pandas.read_excel(dst)
-    frame = pandas.concat([load, frame], ignore_index=True)
-    load = None
+for row in data:
+    ws.append(list(row.values()))
 
-frame.to_excel(dst, freeze_panes=(1, 0), index=False, sheet_name='korStat')
-
-if not prev:
-    exit()
-
-# Remove duplicates
-frame = pandas.read_excel(dst)
-frame.drop_duplicates(inplace=True)
-frame.to_excel(dst, freeze_panes=(1, 0), index=False, sheet_name='korStat')
+wb.save(dst)
