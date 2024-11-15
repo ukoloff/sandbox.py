b43cce6fb1a3f09d035dac914e70fa131a6a0b00
Stas Ukolov <ukoloff@gmail.com>
2024-11-15 10:20:41 +0500

    Extract row writing

--- z23/korstat/korstat.py
+++ z23/korstat/korstat.py
@@ -24,6 +24,21 @@ def json_serial(obj):
     raise TypeError("Type %s not serializable" % type(obj))
 
 
+def add(sheet, data, header=False):
+    if not header:
+        sheet.append(data)
+        return
+    row = []
+    font = openpyxl.styles.Font(bold=True)
+    align = openpyxl.styles.Alignment(horizontal="center")
+    for it in data:
+        cell = openpyxl.cell.WriteOnlyCell(sheet, value=it)
+        cell.font = font
+        cell.alignment = align
+        row.append(cell)
+    sheet.append(row)
+
+
 if not os.path.isfile(src):
     print("File not found: ", src)
     exit()
@@ -35,8 +50,11 @@ wso.freeze_panes = "A2"
 
 wbi = openpyxl.load_workbook(dst, read_only=True, data_only=True)
 wsi = wbi.active
+first = True
 for row in wsi:
-    wso.append([openpyxl.cell.WriteOnlyCell(wso, value=cell.value) for cell in row])
+    data = [cell.value for cell in row]
+    add(wso, data, first)
+    first = False
     # print(json.dumps([cell.value for cell in row], ensure_ascii=False, default=json_serial))
 wbi.close()
 
@@ -44,16 +62,9 @@ data = DBF(src, encoding="cp866")
 first = True
 for row in data:
     if first:
-        header = []
-        font = openpyxl.styles.Font(bold=True)
-        align = openpyxl.styles.Alignment(horizontal="center")
-        for name in row.keys():
-            cell = openpyxl.cell.WriteOnlyCell(wso, value=name)
-            cell.font = font
-            cell.alignment = align
-            header.append(cell)
-        wso.append(header)
-        first = False
-    wso.append(list(row.values()))
+      add(wso, list(row.keys()), first)
+      first = False
+    row = list(row.values())
+    add(wso, row)
 
 wbo.save(dst)
