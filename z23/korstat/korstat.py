b100d85d0c5bd3f907889aef2b068a84c478da09
Stas Ukolov <ukoloff@gmail.com>
2024-11-15 11:06:46 +0500

    Recreate missing XLSX

--- z23/korstat/korstat.py
+++ z23/korstat/korstat.py
@@ -3,6 +3,7 @@
 #
 import os
 import json
+import hashlib
 from datetime import date, datetime
 import openpyxl
 from dbfread import DBF
@@ -24,6 +25,12 @@ def json_serial(obj):
     raise TypeError("Type %s not serializable" % type(obj))
 
 
+def hash(data):
+    return hashlib.sha256(
+        json.dumps(data, ensure_ascii=False, default=json_serial)
+    ).hexdigest()
+
+
 if not os.path.isfile(src):
     print("File not found: ", src)
     exit()
@@ -32,6 +39,7 @@ wbo = openpyxl.Workbook(write_only=True)
 wso = wbo.create_sheet()
 wso.title = "korStat"
 wso.freeze_panes = "A2"
+
 
 def add(data, header=False):
     if not header:
@@ -47,23 +55,21 @@ def add(data, header=False):
         row.append(cell)
     wso.append(row)
 
-
-wbi = openpyxl.load_workbook(dst, read_only=True, data_only=True)
-wsi = wbi.active
 first = True
-for row in wsi:
-    data = [cell.value for cell in row]
-    add(data, first)
-    first = False
-    # print(json.dumps([cell.value for cell in row], ensure_ascii=False, default=json_serial))
-wbi.close()
+if os.path.isfile(dst):
+  wbi = openpyxl.load_workbook(dst, read_only=True, data_only=True)
+  wsi = wbi.active
+  for row in wsi:
+      data = [cell.value for cell in row]
+      add(data, first)
+      first = False
+  wbi.close()
 
 data = DBF(src, encoding="cp866")
-first = True
 for row in data:
     if first:
-      add(list(row.keys()), first)
-      first = False
+        add(list(row.keys()), first)
+        first = False
     row = list(row.values())
     add(row)
 
