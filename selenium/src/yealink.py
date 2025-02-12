57a2f0f18c71be8c6922fc12016b2622a3acb0d9
Stas Ukolov <ukoloff@gmail.com>
2025-02-12 10:02:07 +0500

    Enter credentials

--- selenium/src/yealink.py
+++ selenium/src/yealink.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 
 opts = webdriver.ChromeOptions()
@@ -9,5 +10,13 @@ opts.add_argument("--ignore-certificate-errors")
 
 with webdriver.Chrome(options=opts) as browser:
   browser.get("https://10.33.102.80/")
+
+  user = browser.find_element(By.ID, 'idUsername')
+  pasw = browser.find_element(By.ID, 'idPassword')
+  post = browser.find_element(By.ID, 'idConfirm')
+
+  user.send_keys('admin')
+  pasw.send_keys('qwerty')
+  post.click()
 
   time.sleep(5)
