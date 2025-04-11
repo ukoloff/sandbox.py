4302fe244678da14ef1a57ed863928a776f06bd1
Stas Ukolov <ukoloff@gmail.com>
2025-04-11 10:29:51 +0500

    Test socks5 connection

---
+++ selenium/video/socks5.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+
+opts = webdriver.ChromeOptions()
+opts.add_argument("--proxy-server=socks5://localhost:1080")
+with webdriver.Chrome(options=opts) as browser:
+  browser.implicitly_wait(5)
+
+  browser.get("https://nc.ekb.ru/omz/test/")
+
+  print(browser)
