b6b8e6592163d126423befcc55fbb4787d86143c
Stas Ukolov <ukoloff@gmail.com>
2025-04-16 10:20:02 +0500

    Use socks5

--- selenium/video/socks5ff.py
+++ selenium/video/socks5ff.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
 
-opts = webdriver.ChromeOptions()
-# opts.add_argument("--proxy-server=socks5://localhost:1080")
-with webdriver.Firefox() as browser:
+options = webdriver.FirefoxOptions()
+options.set_preference('network.proxy.type', 1)
+options.set_preference('network.proxy.socks', 'localhost')
+options.set_preference('network.proxy.socks_port', 1080)
+options.set_preference('network.proxy.socks_remote_dns', True)
+with webdriver.Firefox(options=options) as browser:
   browser.implicitly_wait(5)
 
   browser.get("https://nc.ekb.ru/omz/test/")
