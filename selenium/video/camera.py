6f36608842fd1d840074bc1e3b29cc4e1c9e049b
Stas Ukolov <ukoloff@gmail.com>
2025-04-16 10:32:59 +0500

    Send credentials

--- selenium/video/camera.py
+++ selenium/video/camera.py
@@ -1,14 +1,28 @@
+import os
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+env = dict(
+    line.strip().split("=", 2) for line in open(os.path.join(__file__, "../.env"))
+)
 
 options = webdriver.FirefoxOptions()
-options.set_preference('network.proxy.type', 1)
-options.set_preference('network.proxy.socks', 'localhost')
-options.set_preference('network.proxy.socks_port', 1080)
-options.set_preference('network.proxy.socks_remote_dns', True)
+options.set_preference("network.proxy.type", 1)
+options.set_preference("network.proxy.socks", "localhost")
+options.set_preference("network.proxy.socks_port", 1080)
+options.set_preference("network.proxy.socks_remote_dns", True)
 
 with webdriver.Firefox(options=options) as browser:
-  browser.implicitly_wait(5)
+    browser.implicitly_wait(5)
 
-  browser.get("http://192.168.0.21")
+    browser.get("http://192.168.0.21")
 
-  print(browser)
+    u = browser.find_element(By.ID, "login_user")
+    u.clear()
+    u.send_keys(env['USER'])
+    p = browser.find_element(By.ID, "login_psw")
+    p.clear()
+    p.send_keys(env['PASS'])
+
+
+    print(browser)
