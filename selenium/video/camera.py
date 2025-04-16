70bdc5e8b4996e982679bdb0438a23137d939c5d
Stas Ukolov <ukoloff@gmail.com>
2025-04-16 14:58:15 +0500

    Log progress

--- selenium/video/camera.py
+++ selenium/video/camera.py
@@ -1,9 +1,15 @@
 import os
 import time
 import traceback
+from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+
+logfile = os.path.join(
+    __file__, "../logs", f"camera-{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.log"
+)
+logfile = open(logfile, "w", encoding="utf-8")
 
 env = dict(
     line.strip().split("=", 2) for line in open(os.path.join(__file__, "../.env"))
@@ -17,19 +23,24 @@ options.set_preference("network.proxy.socks_remote_dns", True)
 
 
 def camera(browser, ip):
-    print(f"Connecting to {ip}")
+    def log(str):
+        msg = f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}]<{ip}>\t{str}"
+        print(msg)
+        print(msg, file=logfile)
+
+    log("Подключаемся...")
     try:
         try:
             browser.get(f"http://{ip}")
         except Exception:
-            print("Ошибка соединения")
+            log("ERROR: Ошибка соединения")
             return
 
-        print("Connected")
+        log("Подключились")
 
         u = browser.find_elements(By.ID, "login_user")
         if not len(u):
-            print("Неподдерживаемая форма авторизации")
+            log("ERROR: Неподдерживаемая форма авторизации")
             return
         u = u[0]
         u.clear()
@@ -40,7 +51,7 @@ def camera(browser, ip):
         b = browser.find_element(By.LINK_TEXT, "Вход")
         b.click()
 
-        print("Открываю Настройки")
+        log("Открываю Настройки")
 
         m = browser.find_element(By.CSS_SELECTOR, "span[title=Настройки]")
         time.sleep(1)
@@ -49,13 +60,13 @@ def camera(browser, ip):
         time.sleep(1)
         m.click()
 
-        print("Меняю настройки")
+        log("Меняю настройки")
 
         s = browser.find_element(By.ID, "video_compression0")
         s = Select(s)
         h264 = "H.264"
         if not len([1 for o in s.options if o.text == h264]):
-            print(f"Вариант {h264} недоступен")
+            log(f"ERROR: Вариант {h264} недоступен")
             return
         s.select_by_visible_text(h264)
 
@@ -75,29 +86,29 @@ def camera(browser, ip):
         i.clear()
         # i.send_keys('12')
 
-        print("Сохраняю изменения")
+        log("Сохраняю изменения")
 
         b = browser.find_element(By.LINK_TEXT, "Сохранить")
         b.click()
 
         bs = browser.find_elements(By.LINK_TEXT, "OK")
         if len(bs):
-            print("Proceed to reboot")
+            log("Перезагрузка...")
             bs[0].click()
             time.sleep(1)
 
     except Exception as e:
         tb = traceback.extract_tb(e.__traceback__)
         pos = [z for z in tb if z.filename == __file__][-1]
-        print(f"Ошибка в строке {pos.lineno}: {pos._lines.strip()}")
+        log(f"ERROR: Ошибка в строке {pos.lineno}: {pos._lines.strip()}")
     finally:
-        print(f"Done with {ip}")
+        log("Отключение")
 
 
 with webdriver.Firefox(options=options) as browser:
     browser.set_page_load_timeout(5)
     browser.implicitly_wait(5)
 
-    camera(browser, "192.168.0.20")
+    camera(browser, "192.168.0.23")
 
     print("That's all folks!")
