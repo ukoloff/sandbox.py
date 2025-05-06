9b4dfd90a3ad44ad40937333a8faaa088a00ea09
Stas Ukolov <ukoloff@gmail.com>
2025-05-06 16:43:54 +0500

    Restart after long pause

---
+++ selenium/src/6/6.1.3.py
@@ -0,0 +1,9 @@
+#
+# https://stepik.org/lesson/732059/step/3?unit=733592
+#
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+with webdriver.Chrome() as browser:
+    browser.implicitly_wait(5)
+    browser.get("https://parsinger.ru/selenium/6/6.2/index.html")
