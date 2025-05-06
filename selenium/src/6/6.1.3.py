baf29ae3d0dba4f12de29ac1a2fd9ca6d67c76fd
Stas Ukolov <ukoloff@gmail.com>
2025-05-06 16:46:20 +0500

    Double click

--- selenium/src/6/6.1.3.py
+++ selenium/src/6/6.1.3.py
@@ -7,3 +7,8 @@ from selenium.webdriver.common.by import By
 with webdriver.Chrome() as browser:
     browser.implicitly_wait(5)
     browser.get("https://parsinger.ru/selenium/6/6.2/index.html")
+    link = browser.find_element(By.TAG_NAME, 'a')
+    link.click()
+    link = browser.find_element(By.TAG_NAME, 'a')
+    link.click()
+    a = 1
