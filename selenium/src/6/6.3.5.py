0f751c500b65505f2e8159c4a4ff3310ad9cb8a7
Stas Ukolov <ukoloff@gmail.com>
2025-05-07 13:10:02 +0500

    Enumerate cookies

---
+++ selenium/src/6/6.3.5.py
@@ -0,0 +1,19 @@
+#
+# https://stepik.org/lesson/732061/step/5?unit=733594
+#
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+svc = webdriver.ChromeService(
+    executable_path=r"C:\Users\s.ukolov\.cache\selenium\chromedriver\win64\135.0.7049.114\chromedriver.exe"
+)
+
+with webdriver.Chrome(service=svc) as browser:
+    browser.implicitly_wait(5)
+    browser.get("https://parsinger.ru/selenium/6/6.3/index.html")
+
+    for cookie in browser.get_cookies():
+      if cookie['value'] == 'true':
+         song = cookie['name']
+
+    print(song)
