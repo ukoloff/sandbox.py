55a45e639a18bda3cd84d3bfcc025f0c3bb305d0
Stas Ukolov <ukoloff@gmail.com>
2025-05-07 16:37:38 +0500

    Disable workaround

--- selenium/src/6/6.6.2.py
+++ selenium/src/6/6.6.2.py
@@ -4,18 +4,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-svc = webdriver.ChromeService(
-    executable_path=r"C:\Users\s.ukolov\.cache\selenium\chromedriver\win64\135.0.7049.114\chromedriver.exe"
-)
-
-with webdriver.Chrome(service=svc) as browser:
+with webdriver.Chrome() as browser:
     browser.implicitly_wait(5)
     browser.get("https://parsinger.ru/selenium/5.5/1/1.html")
 
-    for i in browser.find_elements(By.CLASS_NAME, 'text-field'):
+    for i in browser.find_elements(By.CLASS_NAME, "text-field"):
         i.clear()
 
-    browser.find_element(By.TAG_NAME, 'button').click()
+    browser.find_element(By.TAG_NAME, "button").click()
 
     answer = browser.switch_to.alert.text
     print(answer)
