fc30245fade37ab636a0775a061adb6c2ea9a96f
Stas Ukolov <ukoloff@gmail.com>
2025-05-12 13:49:39 +0500

    Unpin version

--- selenium/src/6/6.6.11.py
+++ selenium/src/6/6.6.11.py
@@ -5,17 +5,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 
-svc = webdriver.ChromeService(
-    executable_path=r"C:\Users\s.ukolov\.cache\selenium\chromedriver\win64\135.0.7049.114\chromedriver.exe"
-)
-
-with webdriver.Chrome(service=svc) as browser:
+with webdriver.Chrome() as browser:
     browser.implicitly_wait(5)
     browser.get("https://parsinger.ru/methods/3/index.html")
 
     summa = 0
     for c in browser.get_cookies():
-        if not c['name'].startswith('secret_cookie_'):
+        if not c["name"].startswith("secret_cookie_"):
             continue
-        summa += int(c['value'])
+        summa += int(c["value"])
     print(summa)
