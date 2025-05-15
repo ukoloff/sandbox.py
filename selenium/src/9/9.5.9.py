14d17f8d9673ff38ee51a736924d191536c63d56
Stas Ukolov <ukoloff@gmail.com>
2025-05-15 13:59:55 +0500

    Unpin

--- selenium/src/9/9.5.9.py
+++ selenium/src/9/9.5.9.py
@@ -7,11 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-svc = webdriver.ChromeService(
-    executable_path=r"C:\Users\s.ukolov\.cache\selenium\chromedriver\win64\135.0.7049.114\chromedriver.exe"
-)
-
-with webdriver.Chrome(service=svc) as browser:
+with webdriver.Chrome() as browser:
     browser.implicitly_wait(1)
     browser.get("https://parsinger.ru/selenium/9/9.5.3/index.html")
 
