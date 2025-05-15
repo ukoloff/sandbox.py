455bcefcbb429f595a2fbbbfc3a1d34833f84af9
Stas Ukolov <ukoloff@gmail.com>
2025-05-15 10:05:22 +0500

    Wait for URL

--- selenium/src/9/9.3.3.py
+++ selenium/src/9/9.3.3.py
@@ -3,8 +3,6 @@
 #
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 svc = webdriver.ChromeService(
     executable_path=r"C:\Users\s.ukolov\.cache\selenium\chromedriver\win64\135.0.7049.114\chromedriver.exe"
