2a759bdaa3cbacdfb31176c88dbf7d2d6759189a
Stas Ukolov <ukoloff@gmail.com>
2025-06-04 16:40:19 +0500

    Get GigaChat embeddings

--- ai/src/giga0.py
+++ ai/src/giga0.py
@@ -1,10 +1,15 @@
 from dotenv import load_dotenv
 from gigachat import GigaChat
+# https://github.com/ai-forever/gigachat
 
 load_dotenv()
 
 llm = GigaChat(verify_ssl_certs=False)
 
-
-q = llm.chat('Однажды в студёную зимнюю пору?')
+# Query
+q = llm.chat("Однажды в студёную зимнюю пору?")
 print(q.choices[0].message.content)
+
+# Embedding
+emb = llm.embeddings(["Однажды в студёную зимнюю пору?"])
+print(emb)
