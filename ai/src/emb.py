142d6cacb05d2c022d52b4a227646ff7aca73392
Stas Ukolov <ukoloff@gmail.com>
2025-06-05 09:29:59 +0500

    Remove deprecation warning

--- ai/src/emb.py
+++ ai/src/emb.py
@@ -1,9 +1,10 @@
 #
 # Free embeddings
 #
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 
 emb = HuggingFaceEmbeddings(model_name='all-MiniLM-L6-v2')
 
 res = emb.embed_query("Однажды в студёную зимнюю пору")
 print(res)
+print(len(res))
