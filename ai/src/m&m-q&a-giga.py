6fef244536bd26667914f42ac70bd401dabb1814
Stas Ukolov <ukoloff@gmail.com>
2025-06-06 13:27:20 +0500

    Pass to LLM

--- ai/src/m&m-q&a-giga.py
+++ ai/src/m&m-q&a-giga.py
@@ -4,7 +4,7 @@
 from pathlib import Path
 from langchain_chroma import Chroma
 from dotenv import load_dotenv
-from gigachat import GigaChat
+from langchain_gigachat.chat_models import GigaChat
 from langchain_gigachat import GigaChatEmbeddings
 from langchain_core.runnables import RunnablePassthrough
 from langchain import hub
@@ -27,10 +27,15 @@ prompt = hub.pull("rlm/rag-prompt")
 
 retriever = db.as_retriever()
 
-chain = {
-    "context": retriever | format_docs,
-    "question": RunnablePassthrough(),
-} | prompt
+chain = (
+    {
+        "context": retriever | format_docs,
+        "question": RunnablePassthrough(),
+    }
+    | prompt
+    | llm
+)
+
 
 res = chain.invoke(question)
 print(res)
