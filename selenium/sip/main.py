56366bc8099a87d81d4cb639d29d139203ed8a68
Stas Ukolov <ukoloff@gmail.com>
2025-07-29 13:09:00 +0500

    Connect to Chrome

--- selenium/sip/main.py
+++ selenium/sip/main.py
@@ -1,11 +1,20 @@
 from os.path import join, dirname
 from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 
 load_dotenv(join(dirname(__file__), ".env"))
 
 
+def tel(ip):
+    chrome_options = Options()
+    chrome_options.add_argument("--disable-auto-update")
+    with webdriver.Chrome(options=chrome_options) as browser:
+      browser.get(f"https://{ip}")
+
 def main():
-    print("Hello from sip!")
+   tel('10.172.202.31')
 
 
 if __name__ == "__main__":
