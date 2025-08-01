c88a8b18998a6b231d822f5f0c623cfd88dcc9a6
Stas Ukolov <ukoloff@gmail.com>
2025-08-01 08:34:29 +0500

    Add ping3

--- selenium/sip/main.py
+++ selenium/sip/main.py
@@ -19,32 +19,42 @@ def tel(ip):
         browser.implicitly_wait(3)
         browser.get(f"https://{ip}")
 
-        login = browser.find_elements(By.ID, 'idUsername')
+        login = browser.find_elements(By.ID, "idUsername")
         if len(login) != 1:
             print("Authorization form not found")
-        login[0].send_keys(os.getenv('SIP_USER'))
-        browser.find_element(By.ID, 'idPassword').send_keys(os.getenv('SIP_PASS'))
-        browser.find_element(By.ID, 'idConfirm').click()
+        login[0].send_keys(os.getenv("SIP_USER"))
+        browser.find_element(By.ID, "idPassword").send_keys(os.getenv("SIP_PASS"))
+        browser.find_element(By.ID, "idConfirm").click()
 
-        browser.find_element(By.ID, 'Account').click()
+        browser.find_element(By.ID, "Account").click()
 
-        for name in 'AccountLabel AccountRegisterName AccountUserName'.split():
-            txt = browser.find_element(By.NAME, name).get_attribute('value')
+        for name in "AccountLabel AccountRegisterName AccountUserName".split():
+            txt = browser.find_element(By.NAME, name).get_attribute("value")
             print(txt)
 
 
-def main():
+def readCSV():
     global exts
     exts = readCSV()
-    net = ipaddress.ip_network('10.172.200.0/22')
-    hosts = [*net.hosts()]
+
+
+def walkIPs(network="10.172.200.0/22"):
+    for ip in ipaddress.ip_network(network).hosts():
+        print(ip)
+
+
+def main():
+    readCSV()
+    walkIPs()
     tel("10.172.202.31")
+
 
 def readCSV():
     src = join(dirname(__file__), "data", "omz2sinara.csv")
     with open(src) as f:
-        reader = csv.DictReader(f, delimiter=';')
-        return dict((row['Внутр. Номер'], row) for row in reader)
+        reader = csv.DictReader(f, delimiter=";")
+        return dict((row["Внутр. Номер"], row) for row in reader)
+
 
 if __name__ == "__main__":
     main()
