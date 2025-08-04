a30295c6b3bda06e1ac2e8a0bdbe03ba0b05a65a
Stas Ukolov <ukoloff@gmail.com>
2025-08-04 16:09:43 +0500

    Upgrade DIDs

--- selenium/sip/upgrade.py
+++ selenium/sip/upgrade.py
@@ -4,13 +4,30 @@
 import csv
 from os.path import join, dirname
 
+data = join(dirname(__file__), "data")
+
 
 def readCSV():
-    src = join(dirname(__file__), "data", "omz2sinara.csv")
+    src = join(data, "omz2sinara.csv")
     with open(src) as f:
         reader = csv.DictReader(f, delimiter=";")
         return dict((row["extension"], row) for row in reader)
 
+def did(file='dids'):
+    with open(join(data, f"{file}.csv"), encoding='utf-8') as f:
+        did = [*csv.DictReader(f)]
+    with open(join(data, f"{file}.sinara.csv"), 'w', newline='', encoding='utf-8') as f:
+      writer = csv.DictWriter(f, fieldnames=[*did[0].keys()])
+      writer.writeheader()
+      for z in did:
+          pfx, ext, tail = z['destination'].split(',', 2)
+          if ext not in extensions:
+              continue
+          z['destination'] = ','.join([pfx, extensions[ext]['extension new'], tail])
+          writer.writerow(z)
+
+
 
 extensions = readCSV()
-print(extensions)
+print("Upgrading DIDs...")
+did()
